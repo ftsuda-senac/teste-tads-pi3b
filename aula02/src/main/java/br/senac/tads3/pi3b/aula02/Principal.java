b442946abd961ddd7a7cf4b64b7f1796f32e75a3
Fernando Tsuda <ftsuda.senac@gmail.com>
2017-08-15 21:31:34 -0300

    Alteração 10x

--- aula02/src/main/java/br/senac/tads3/pi3b/aula02/Principal.java
+++ aula02/src/main/java/br/senac/tads3/pi3b/aula02/Principal.java
@@ -15,7 +15,9 @@ public class Principal {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    System.out.println("Hello world PI3B!!!");
+    for (int i = 0; i < 10; i++) {
+        System.out.println("Hello world PI3B!!!");
+    }
   }
   
 }
